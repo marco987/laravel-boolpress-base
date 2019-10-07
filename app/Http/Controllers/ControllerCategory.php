7eaef120219c7665879b223da9d7d80405d98e24
marco987 <pacemarco87@gmail.com>
2019-10-07 15:02:51 +0200

    View base + CSS base

--- app/Http/Controllers/ControllerCategory.php
+++ app/Http/Controllers/ControllerCategory.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
 
 class ControllerCategory extends Controller
 {
-    //
+  public function index(){
+
+    return view('home-page');
+  }
 }
