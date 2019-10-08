c718a9206f18d02fb93370c0d65f541ae569d564
marco987 <pacemarco87@gmail.com>
2019-10-08 13:49:51 +0200

    Back home

--- resources/views/category-home.blade.php
+++ resources/views/category-home.blade.php
@@ -5,6 +5,7 @@
   <div class="box-categoria">
     <h2>Categoria {{$category -> category}}</h2>
     <p class="category-color" style="background-color:{{$category -> category}}"></p>
+    <a href="{{Route('home-page')}}">Back home</a>
 
     @foreach ($posts as $post)
 
@@ -12,6 +13,7 @@
         <h5>{{$post -> title}}</h5>
         <p>{{$post -> text}}</p>
       </div>
+
     @endforeach
 
   </div>
